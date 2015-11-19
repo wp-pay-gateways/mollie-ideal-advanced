6e5eec07bdedc8760c5d8f3f4d2dfe1322516802
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 13:39:18 +0100

    Extend from iDEAL Advanced library.

--- src/Integration.php
+++ src/Integration.php
@@ -1,23 +1,11 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_Mollie_IDealAdvanced_Integration {
+class Pronamic_WP_Pay_Gateways_Mollie_IDealAdvanced_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvanced_AbstractIntegration {
 	public function __construct() {
 		$this->id            = 'mollie-ideal-advanced';
 		$this->name          = 'Mollie - iDEAL Advanced';
 		$this->dashboard_url = 'http://www.mollie.nl/';
 		$this->provider      = 'mollie';
 		$this->deprecated    = true;
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvanced_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvanced_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_IDealAdvanced_Gateway';
 	}
 }
