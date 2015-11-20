69cbcc3a57406cb3030233b251bf20020d8eb58a
Remco Tolsma <info@remcotolsma.nl>
2015-11-20 13:15:16 +0100

    Added an custom config factory.

--- src/Integration.php
+++ src/Integration.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Title: Mollie - iDEAL Advanced integration
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2015
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
 class Pronamic_WP_Pay_Gateways_Mollie_IDealAdvanced_Integration extends Pronamic_WP_Pay_Gateways_IDealAdvanced_AbstractIntegration {
 	public function __construct() {
 		$this->id            = 'mollie-ideal-advanced';
@@ -7,5 +15,9 @@ class Pronamic_WP_Pay_Gateways_Mollie_IDealAdvanced_Integration extends Pronamic
 		$this->dashboard_url = 'http://www.mollie.nl/';
 		$this->provider      = 'mollie';
 		$this->deprecated    = true;
+	}
+
+	public function get_config_factory_class() {
+		return 'Pronamic_WP_Pay_Gateways_Mollie_IDealAdvanced_ConfigFactory';
 	}
 }
